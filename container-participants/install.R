79838574de08f750b2609458fd824302429abbf4
Steffen Neumann <sneumann@ipb-halle.de>
2017-02-02 09:27:33 +0100

    Opens MTBLS2

---
+++ container-participants/install.R
@@ -0,0 +1,6 @@
+install.packages(c("miniCRAN","igraph","magrittr","networkD3",
+                   "visNetwork","magrittr","rmarkdown", "formatR",
+		   "tm", "wordcloud", "SnowballC"), 
+                 repos="https://cran.r-project.org")
+
+
